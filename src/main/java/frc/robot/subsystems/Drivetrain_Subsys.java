eac1514fe1114a1e5e6accb574616628c77769d9
andrew-cua <andrew@cuafamily.com>
2019-01-01 13:39:43 -0500

    added NavX

--- src/main/java/frc/robot/subsystems/Drivetrain_Subsys.java
+++ src/main/java/frc/robot/subsystems/Drivetrain_Subsys.java
@@ -7,16 +7,20 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.commands.*;
 /**
  * Add your docs here.
  */
-public class Drivetrain_Subsys extends Subsystem {
+public class Drivetrain_Subsys extends PIDSubsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
@@ -26,14 +30,19 @@ public class Drivetrain_Subsys extends Subsystem {
                 backLeft,
                 frontRight,
                 backRight;
-
+  private AHRS navX;
   private boolean inverted = true;
   private Drivetrain_Subsys()
   {
+    super("Drivetrain", 2.0,0,0);
+    setAbsoluteTolerance(0.5);
+    getPIDController().setContinuous(false);
     this.frontLeft  = new Spark(RobotMap.leftFrontMotor);
     this.backLeft   = new Spark(RobotMap.leftBackMotor);
     this.frontRight = new Spark(RobotMap.rightFrontMotor);
     this.backRight  = new Spark(RobotMap.rightBackMotor);
+
+    this.navX  = new AHRS(SerialPort.Port.kMXP);
 
     this.frontRight.setInverted(inverted);
     this.backRight.setInverted(inverted);
@@ -77,6 +86,24 @@ public class Drivetrain_Subsys extends Subsystem {
   {
     power(1,1);
     System.out.print(frontLeft.get() + frontRight.get());
+  }
+
+  private void PIDwrite(double output)
+  {
+    frontLeft.pidWrite(output);
+    backLeft.pidWrite(output);
+    frontRight.pidWrite(output);
+    backRight.pidWrite(output);
+  }
+
+  protected double returnPIDInput()
+  {
+    return navX.getAngle();
+  }
+
+  protected void usePIDOutput(double output)
+  {
+    PIDwrite(output);
   }
 
   public static Drivetrain_Subsys getInstance()
